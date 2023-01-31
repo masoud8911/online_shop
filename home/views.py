f77dd153935ceaeb1dda953e2f9cd687a06b41e7
masoud8911 <masoud.shakarami89@gmail.com>
2023-01-31 20:46:49 +0330

    Add subcategory for products

--- home/views.py
+++ home/views.py
@@ -6,9 +6,9 @@ from .models import Product, Category
 class HomeView(View):
     def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
-        categories = Category.objects.all()
+        categories = Category.objects.filter(is_sub=False)
         if category_slug:
-            category = categories.get(slug=category_slug)
+            category = Category.objects.get(slug=category_slug)
             products = products.filter(category=category)
         return render(request, 'home/index.html', {'products': products, 'categories': categories})
 
