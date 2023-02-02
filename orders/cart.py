ae9528dab061721e78c1ed782624be66dfc935d9
masoud8911 <masoud.shakarami89@gmail.com>
2023-02-02 19:24:46 +0330

    Add remove order in customer cart

--- orders/cart.py
+++ orders/cart.py
@@ -15,7 +15,7 @@ class Cart:
         products = Product.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
         for product in products:
-            cart[str(product.id)]['product']=product.name
+            cart[str(product.id)]['product']=product
 
         for item in cart.values():
             item['total_price']=int(item['price'])*item['quantity']
@@ -28,5 +28,18 @@ class Cart:
         self.cart[product_id]['quantity'] += quantity
         self.save()
 
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
     def save(self):
         self.session.modified = True
+
+    def get_total_price(self):
+        return sum(int(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        del self.session[CART_SESSION_ID]
+        self.save()
--- orders/views.py
+++ orders/views.py
@@ -13,8 +13,16 @@ class CartView(View):
 class CartAddView(View):
     def post(self, request, product_id):
         cart = Cart(request)
-        product = get_object_or_404(Product, pk=product_id)
+        product = get_object_or_404(Product, id=product_id)
         form = CartAddForm(request.POST)
         if form.is_valid():
             cart.add(product, form.cleaned_data['quantity'])
+        return redirect('orders:cart')
+
+
+class CartRemoveView(View):
+    def get(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        cart.remove(product)
         return redirect('orders:cart')
