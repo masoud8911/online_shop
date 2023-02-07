5c35be076e0b1f988ae2eac741ef532c6e7f1498
masoud8911 <masoud.shakarami89@gmail.com>
2023-02-07 18:21:32 +0330

    Add coupon for order

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Coupon
 
 
 class OrderItemInline(admin.TabularInline):
@@ -13,3 +13,5 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ('paid',)
     inlines = (OrderItemInline,)
 
+
+admin.site.register(Coupon)
