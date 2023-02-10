2225919f79f3f0b1985d75e6c29430436db7a5c2
masoud8911 <masoud.shakarami89@gmail.com>
2023-02-10 16:31:24 +0330

    Connect to postgresql

--- M/settings.py
+++ M/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'M.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
 
